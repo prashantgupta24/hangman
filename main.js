7c3250c9aa8b0bc9a9e6e2f7b248e610b6d80430
Prashant Gupta <prashantgupta@us.ibm.com>
2017-06-08 23:34:43 -0700

    major refactoring

--- main.js
+++ main.js
@@ -1,62 +1,73 @@
 $(function() {
-  let mainName = 'wonder woman';
-  let blankName = '';
-  let playing = true;
 
-  $('#chances').text(3);
-  let canvas = document.getElementById('wordCanvas');
-  let ctx = canvas.getContext('2d');
-  ctx.font = '30px Arial';
+  let game = {
+    movieNames: ['wonder woman', 'monty python and the holy grail', 'being john malkovich'],
+    movieName: '',
+    playing: true,
+    blankMovieName: '',
+    chances: 5,
+    initialize: function () {
+      this.canvas.clearCanvas();
+      this.blankMovieName = '';
+      $('#chances').text(this.chances);
+      updateTextInDom('#incorrect', '');
+      this.chooseRandomMovie(),
+      this.canvas.initializeCanvas();
+      this.updateBlankName();
+      drawBlanks();
+    },
+    chooseRandomMovie: function () {
+      this.movieName = this.movieNames[Math.floor(Math.random()*this.movieNames.length)];
+      //console.log(this.movieName);
+    },
+    updateBlankName: function () {
+      for(let i=0;i<this.movieName.length;i++) {
+        if(this.movieName.charAt(i) === (' ')) {
+          this.blankMovieName += '/';
+        } else {
+          this.blankMovieName  += '_';
+        }
+      }
+      //console.log(this.blankMovieName);
+    },
+    canvas: {
+      canvasElement: document.getElementById('wordCanvas'),
+      ctx: document.getElementById('wordCanvas').getContext('2d'),
+      initializeCanvas: function() {
+        this.ctx.font = '30px Arial';
+      },
+      getWidthOfCanvas: function () {
+        return this.canvasElement.width;
+      },
+      clearCanvas: function () {
+        this.ctx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+        //console.log('canvas cleared');
+      }
+    }
+  };
 
-  updateBlankName(mainName);
+  game.initialize();
 
   $(document).keypress(function (event) {
-    if(playing) {
-      var keyPressed = String.fromCharCode(event.which || event.keyCode);
+    if(game.playing) {
       $('#note').text('');
-      if(mainName.indexOf(keyPressed) > -1) {
-        revealChar(keyPressed);
+      let keyCode = event.which || event.keyCode;
+      //console.log(keyCode);
+      if((keyCode >= 65 && keyCode<=90) || (keyCode>=97 && keyCode<=122)) {
+        processKey(keyCode);
       } else {
-        let curChances = $('#chances').html();
-        let incorrectGuess = $('#incorrect').html().trim();
-        if(incorrectGuess.length > 0){
-          if(incorrectGuess.indexOf(keyPressed) > -1){
-            $('#note').text('You tried \'' + keyPressed + '\' already!');
-          } else {
-            $('#incorrect').text(incorrectGuess + ', ' + keyPressed);
-            $('#chances').text(--curChances);
-          }
-        } else {
-          $('#incorrect').text(keyPressed);
-          $('#chances').text(--curChances);
-        }
-        if(curChances == 0) {
-          setTimeout(function () {alert('You lost');}, 100);
-          setTimeout(function () {resetPlay();}, 100);
-        }
+        updateTextInDom('#note', 'Please enter alphabets only!');
       }
     }
   });
 
-  function updateBlankName (name) {
-    //console.log(name.length);
-    for(let i=0;i<name.length;i++) {
-      if(name.charAt(i) === (' ')) {
-        blankName += '/';
-      } else {
-        blankName  += '_';
-      }
-    }
-    drawBlanks();
-  }
-
   function drawBlanks() {
-    //console.log(blankName);
+    //console.log(game.blankMovieName);
     let x = 50;
     let y = 50;
-    let canvasWidth = canvas.width;
+    let canvasWidth = game.canvas.getWidthOfCanvas();
 
-    for(let i=0;i<blankName.length;i++) {
+    for(let i=0;i<game.blankMovieName.length;i++) {
       x = 10+(i*50);
       y = 50;
       if(x > canvasWidth) {
@@ -64,29 +75,79 @@ $(function() {
         //console.log(x+' '+y);
         x = x % canvasWidth;
       }
-      ctx.fillText(blankName.charAt(i), x, y);
+      game.canvas.ctx.fillText(game.blankMovieName.charAt(i), x, y);
     }
-    if(blankName.indexOf('_') == -1) {
-      setTimeout(function () {alert('You won!');}, 100);
+    if(game.blankMovieName.indexOf('_') == -1) {
+      setTimeout(function () {resetPlay('won');}, 10);
+    }
+  }
+
+  function resetPlay(gameStatus) {
+    let gameEndMessage = '';
+    if(gameStatus === 'lost') {
+      gameEndMessage = 'You lost! The movie was ' + game.movieName + '. Do you \
+want to continue?';
+    }
+    else {
+      gameEndMessage = 'You won! Good Job! Do you want to continue?';
+
+    }
+    let continueResult = confirm(gameEndMessage);
+    if(continueResult) {
+      //game.initialize();
+      //game.canvas.clearCanvas();
+      game.initialize();
+    } else {
+      game.playing = false;
+    }
+  }
+
+  function updateTextInDom(domElement, text) {
+    $(domElement).text(text);
+  }
+
+  function processKey(keyCode) {
+    let keyPressed = String.fromCharCode(keyCode).toLowerCase();
+    if(game.movieName.indexOf(keyPressed) > -1) {
+      revealChar(keyPressed);
+    } else {
+      wrongGuess(keyPressed);
+    }
+  }
+
+  function wrongGuess (keyPressed) {
+    let curChances = $('#chances').html();
+    let incorrectGuess = $('#incorrect').html().trim();
+
+    if(incorrectGuess.length > 0){
+      if(incorrectGuess.indexOf(keyPressed) > -1){
+        updateTextInDom('#note', 'You tried \'' + keyPressed + '\' already!');
+      } else {
+        updateTextInDom('#incorrect', incorrectGuess + ', ' + keyPressed);
+        updateTextInDom('#chances', --curChances);
+      }
+    } else {
+      updateTextInDom('#incorrect', keyPressed);
+      updateTextInDom('#chances', --curChances);
+    }
+    if(curChances == 0) {
+      setTimeout(function () {resetPlay('lost');}, 10);
     }
   }
 
   function revealChar(char) {
-    let blankNameAsArray = blankName.split('');
-    let i = 0;
-    let indexChar = 0;
+    let blankMovieNameAsArray = game.blankMovieName.split('');
+    let startingIndex = 0;
+    let indexOfChar = 0;
     //console.log('here');
-    while((indexChar = mainName.indexOf(char, i)) > -1) {
-      //console.log('found at : ' + indexChar);
-      blankNameAsArray[indexChar] = char;
-      i = indexChar + 1;
+    while((indexOfChar = game.movieName.indexOf(char, startingIndex)) > -1) {
+      //console.log('found at : ' + indexOfChar);
+      blankMovieNameAsArray[indexOfChar] = char;
+      startingIndex = indexOfChar + 1;
     }
-    blankName = blankNameAsArray.toString().replace(/,/g, '');
-    //console.log(blankName);
+    game.blankMovieName = blankMovieNameAsArray.toString().replace(/,/g, '');
+    //console.log(blankMovieName);
     drawBlanks();
   }
 
-  function resetPlay() {
-    playing = false;
-  }
 });
